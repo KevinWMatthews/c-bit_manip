5868c2d104293a7e5b69a741a818b4ab76fb1ec2
Kevin Matthews <kmatthews@cyberdata.net>
2016-05-31 15:36:00 -0700

    Clear bits/Clear bitmask

--- test/Test_BitManip.cpp
+++ test/Test_BitManip.cpp
@@ -7,10 +7,15 @@ extern "C"
 #include "Test_BitManip.h"
 #include "CppUTest/TestHarness.h"
 
-static uint8_t eightBit;
+
+#define EIGHT_BITS (1<<7) | (1<<6) | (1<<5) | (1<<4) | (1<<3) | (1<<2) | (1<<1) | (1<<0)
+
+
 
 TEST_GROUP(SetBits)
 {
+    uint8_t eightBit;
+
     void setup()
     {
         eightBit = 0;
@@ -23,8 +28,8 @@ TEST_GROUP(SetBits)
 
 TEST(SetBits, ItCanSetAllBitsInABitmask)
 {
-    SET_BITS(eightBit, 0xff);
-    BYTES_EQUAL(0xff, eightBit);
+    SET_BITS(eightBit, EIGHT_BITS);
+    BYTES_EQUAL(EIGHT_BITS, eightBit);
 }
 
 TEST(SetBits, ItDoesNotClearAnyBitsInABitmask)
@@ -54,8 +59,57 @@ TEST(SetBits, ItCanSetTheMostSignificantBit)
 
 
 
+TEST_GROUP(ClearBits)
+{
+    uint8_t eightBit;
+
+    void setup()
+    {
+        eightBit = 0xff;
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(ClearBits, ItCanClearAllBits)
+{
+    CLEAR_BITS(eightBit, EIGHT_BITS);
+    BYTES_EQUAL(0x00, eightBit);
+}
+
+TEST(ClearBits, ItWillNotSetAnyBits)
+{
+    eightBit = 0;
+    CLEAR_BITS(eightBit, EIGHT_BITS);
+    BYTES_EQUAL(0x00, eightBit);
+}
+
+TEST(ClearBits, ItCanClearTheLeastSignificantBit)
+{
+    CLEAR_BITS(eightBit, (1<<0));
+    BYTES_EQUAL(0xfe, eightBit);
+}
+
+TEST(ClearBits, ItCanClearTheMostSignificantBit)
+{
+    CLEAR_BITS(eightBit, (1<<7));
+    BYTES_EQUAL(0x7f, eightBit);
+}
+
+// IGNORE_TEST(SetBits, ThereIsNoProtectionForThis)
+// {
+//     CLEAR_BITS( eightBit, (1<<8) );
+//     BYTES_EQUAL(0xff, eightBit);
+// }
+
+
+
 TEST_GROUP(RightMostBit)
 {
+    uint8_t eightBit;
+
     void setup()
     {
         eightBit = 0;
